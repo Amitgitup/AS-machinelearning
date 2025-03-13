673fd31f2973fde6a5c4f10ea2697bcc79808506
Amitgitup <98737596+Amitgitup@users.noreply.github.com>
2025-03-14 01:14:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ with st.sidebar:
           'body_mass_g':body_mass_g,
           'gender':gender}
   input_df = pd.DataFrame(data, index=[0])
-  input_penguins = pd.concat([input_df, X], axis=1)
+  input_penguins = pd.concat([input_df, X], axis=0)
 
 with st.expander('Input Features'):
   st.write("**Input Penguin**")
