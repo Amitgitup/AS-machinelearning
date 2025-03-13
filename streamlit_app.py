c51c195541c74936475dabe4ad4af7fd378b733d
Amitgitup <98737596+Amitgitup@users.noreply.github.com>
2025-03-13 23:42:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd 
 
 st.title('🤖 Machine Learning App')
 
 st.info('This app builds a machine learning model!')
+
+df = pd.read_csv('https://github.com/Amitgitup/AS-machinelearning/blob/master/penguins_cleaned.csv')
+df
